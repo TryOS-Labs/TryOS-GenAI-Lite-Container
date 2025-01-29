2ae670b1f92a27011107ce280f0c63c8ac6dd3f4
thdihan <tanvirhossain20@iut-dhaka.edu>
2025-01-29 22:31:26 +0600

    Production server added

---
+++ frontend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const path = require("path");
+
+app.use(cors());
+
+// server public ui files to all path
+app.use(express.static("public/dist"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log(`app is listening on port 3000`);
+});
